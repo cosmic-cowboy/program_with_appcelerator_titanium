c0d28b47c1c4f924622e4907ade5842268c70056
hiroo takaku <slgerkamp@gmail.com>
2014-10-12 13:26:59 +0900

    Chapter04 画面遷移アプリ（TabGroup）：担当者一覧ページ、登録ページ作成

--- chapter04/screen_transition_with_tab_group/Resources/ui/common/representativeAddWindow.js
+++ chapter04/screen_transition_with_tab_group/Resources/ui/common/representativeAddWindow.js
@@ -1,20 +1,73 @@
 function representativeAddWindow () {
 	
-
-	// ページには何が必要か
-
-	// 画面を表示するWindow
+	// 担当者登録画面を表示するWindow
+	var repAddwindow = Ti.UI.createWindow({
+		backgroundColor: '#fff',
+		title: L('tanto_add')
+	});
 
 	// 担当者登録ページであることを表示するLabel
+	var repAddLabel = Ti.UI.createLabel({
+		color:'#999',
+		top:10,
+		text:L('tanto_name'),
+		textAlign:'center',
+		width:'auto'
+	});
+	repAddwindow.add(repAddLabel);
 
-	// 担当者入力エリア
+	// 担当者入力フィールド
+	var repAddTextField = Ti.UI.createTextField({
+		value:'',
+		hintText: L('hint_tanto_name'),
+		top:45,
+		width:'60%',
+		borderStyle: Titanium.UI.INPUT_BORDERSTYLE_BEZEL
+	});
+	repAddwindow.add(repAddTextField);
 
 	// 担当者登録ボタン
+	var repAddButton = Ti.UI.createButton({
+		title:L('button_add'),
+		top:90,
+		width:'100',
+		left:110
+	});
 
 	// 担当者登録処理
+	repAddButton.addEventListener('click',function (event) {
 
+		// バリデーション
+		if(repAddTextField.value.length === 0){
+			alert('担当者を入力してください');
+			repAddTextField.focus();
+			return false;
+		}
 
+		// DBへの登録処理
+		// データベースをオープン
+		var db = Ti.Database.open('sampleDB');
+		try{
+			// テーブルがなければテーブルを作成
+			db.execute('CREATE TABLE IF NOT EXISTS representative(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT)');
+			// 登録処理
+			db.execute('INSERT INTO representative(name) VALUES(?)', repAddTextField.value);
 
+		} catch(error){
+			Ti.API.info(error);
+		}
+		// データベースをクローズ
+		db.close();
+
+		// あらかじめ設定されているイベントを呼び出す
+		Ti.App.fireEvent('updateTables');
+		// 登録処理が終わったら、ページを閉じる
+		repAddwindow.close({animated:true});
+	});
+
+	repAddwindow.add(repAddButton);
+
+	return repAddwindow;
 }
 
 // 関数をエクスポート
--- chapter04/screen_transition_with_tab_group/Resources/ui/common/representativeViewWindow.js
+++ chapter04/screen_transition_with_tab_group/Resources/ui/common/representativeViewWindow.js
@@ -1,11 +1,27 @@
 function representativeViewWindow () {
 
 	// 担当者を取得し、Viewとして表示する
-
+	var repViewWin = Ti.UI.createWindow({
+		title : L('tanto_view')
+	});
+	var repView = Ti.UI.createView();
 
 	// グローバルイベントを追加
+	Ti.App.addEventListener('updateTables', function (event) {
+		tableView.setData(getData());
+	});
 
+	// テーブルビューの生成
+	var tableView = Ti.UI.createTableView();
+	tableView.setData(getData());
+	repView.add(tableView);
+
+	tableView.addEventListener('click',function (event) {
+		// TODO 編集ページ
+	});
+	repViewWin.add(repView);
 	
+	return repViewWin;
 }
 
 // 関数をエクスポート
@@ -13,3 +29,36 @@ module.exports = representativeViewWindow;
 
 
 // 担当者取得処理を追加
+var getData = function () {
+
+	// 担当者一覧
+	var rowList = [];
+
+	// データベースをオープン
+	var db = Ti.Database.open('sampleDB');
+
+	try{
+		// 担当者テーブルがなければ作成
+		db.execute('CREATE TABLE IF NOT EXISTS representative(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT)');
+		// 担当者一覧取得
+		var rs = db.execute('SELECT id, name FROM representative');
+		while(rs.isValidRow()){
+			var row = Ti.UI.createTableViewRow({
+				title:rs.fieldByName('name'),
+				rsId :rs.fieldByName('id')
+			});
+
+			rowList.push(row);
+			rs.next();
+		}
+		// ResultSetをクローズ
+		rs.close();
+
+	} catch(error){
+		Ti.API.info(error);
+	}
+	// データベースをクローズ
+	db.close();
+
+	return rowList;
+};
