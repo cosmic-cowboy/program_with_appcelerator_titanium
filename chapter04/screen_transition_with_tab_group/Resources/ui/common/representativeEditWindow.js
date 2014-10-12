58950ac72ef801d853cf6e466873bf894e19bd31
hiroo takaku <slgerkamp@gmail.com>
2014-10-12 14:53:43 +0900

    Chapter04 画面遷移アプリ（TabGroup）：TODO 担当者編集ページ

---
+++ chapter04/screen_transition_with_tab_group/Resources/ui/common/representativeEditWindow.js
@@ -0,0 +1,24 @@
+function representativeEditWindow () {
+	
+	// 担当者登録画面を表示するWindow
+	var repEditwindow = Ti.UI.createWindow({
+		backgroundColor: '#fff',
+		title: L('tanto_edit')
+	});
+
+
+	// 編集画面のラベル
+
+	// 編集用テキストフィールド
+
+	// 編集ボタン、編集処理
+
+	// 削除ボタン、削除処理
+
+
+
+	return repEditwindow;
+}
+
+// 関数をエクスポート
+module.exports = representativeEditWindow;
