3edae6e87334aa54d65f64de516a9dc081174639
hiroo takaku <slgerkamp@gmail.com>
2014-10-12 12:29:55 +0900

    Chapter04 画面遷移アプリ（TabGroup）：TODO 担当者ページ

---
+++ chapter04/screen_transition_with_tab_group/Resources/ui/common/representativeAddWindow.js
@@ -0,0 +1,21 @@
+function representativeAddWindow () {
+	
+
+	// ページには何が必要か
+
+	// 画面を表示するWindow
+
+	// 担当者登録ページであることを表示するLabel
+
+	// 担当者入力エリア
+
+	// 担当者登録ボタン
+
+	// 担当者登録処理
+
+
+
+}
+
+// 関数をエクスポート
+module.exports = representativeAddWindow;
---
+++ chapter04/screen_transition_with_tab_group/Resources/ui/common/representativeViewWindow.js
@@ -0,0 +1,15 @@
+function representativeViewWindow () {
+
+	// 担当者を取得し、Viewとして表示する
+
+
+	// グローバルイベントを追加
+
+	
+}
+
+// 関数をエクスポート
+module.exports = representativeViewWindow;
+
+
+// 担当者取得処理を追加
