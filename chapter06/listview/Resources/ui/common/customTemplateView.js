968a3b7ca25eb233c604c55d8212be51b735b5b9
hiroo takaku <slgerkamp@gmail.com>
2014-10-13 20:25:24 +0900

    Chapter06 Webサービスの利用（ListView入門）：カスタムテンプレートの定義

--- chapter06/listview/Resources/ui/common/customTemplateView.js
+++ chapter06/listview/Resources/ui/common/customTemplateView.js
@@ -1,5 +1,65 @@
 function customTemplateView () {
-	var listView = Ti.UI.createListView();
+
+	var plainTemplate = {
+		childTemplates : [
+			{
+				type:'Ti.UI.Label',
+				bindId:'rowtitle',
+				properties : {
+					left : '10dp',
+					font : {fontSize:'16dp', fontStyle:'italic'}
+				},
+				events : {click : report}
+			},
+			{
+				type:'Ti.UI.Label',
+				bindId:'subtitle',
+				properties : {
+					left : '80dp',
+					font : {fontSize:'10dp'}
+				},
+				events : {click : report}
+			},
+			{
+				type:'Ti.UI.ImageView',
+				bindId:'pic',
+				properties : {
+					image : 'KS_nav_ui.png'
+				},
+				events : {click : report}
+			},
+			{
+				type:'Ti.UI.Button',
+				bindId:'button',
+				properties : {
+					width  : '80dp',
+					height : '30dp',
+					right  : '10dp',
+					title : 'PRESS ME'
+				},
+				events : {click : report}
+			}
+		]
+	};
+
+	function report (event) {
+		Ti.API.info(event.bindId);
+	}
+
+	var listView = Ti.UI.createListView({
+		templates : {'plain':plainTemplate},
+		defaultItemTemplate : 'plain'
+	});
+
+	var data = [];
+	for(var i = 1; i <= 10; i++){
+		data.push({
+			rowtitle:{text : 'Row ' + i},
+			subtitle:{text : 'Sub ' + i}
+		});
+	}
+	var section = Ti.UI.createListSection({items : data});
+	listView.sections = [section];
 	return listView;
 }
 
