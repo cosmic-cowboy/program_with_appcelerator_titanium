3ade847adc69d8631f206011c5a0bf5fcb8abc2c
hiroo takaku <slgerkamp@gmail.com>
2014-10-15 07:55:13 +0900

    Chapter09 Alloyによるアプリ作成（タスク管理）：一覧画面レイアウト作成

--- chapter09/TaskManage/app/controllers/index.js
+++ chapter09/TaskManage/app/controllers/index.js
@@ -1,5 +1,13 @@
-function doClick(e) {
-    alert($.label.text);
-}
+$.index.addEventListener("open",function(e) {
+	Alloy.Globals.currentTab = e.activeTab;
+});
+$.index.addEventListener("focus",function(e) {
+	Alloy.Globals.currentTab = e.tab;
+});
+$.index.addEventListener("close", function () {
+  $.destroy();
+});
+
+Alloy.Collections.task.fetch();
 
 $.index.open();
