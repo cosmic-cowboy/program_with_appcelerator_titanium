64fe348b04ad86302d6274e43a826c32b3379a94
hiroo takaku <slgerkamp@gmail.com>
2014-10-14 23:26:07 +0900

    Chapter09 Alloyによるアプリ作成（タスク管理）：タスクモデル作成

--- chapter09/TaskManage/app/models/task.js
+++ chapter09/TaskManage/app/models/task.js
@@ -1,6 +1,12 @@
 exports.definition = {
 	config: {
-
+		columns : {
+			"what" : "text",
+			"detail" : "text",
+			"limitDate" : "text",
+			"importance" : "integer",
+			"status" : "integer"
+		},
 		adapter: {
 			type: "sql",
 			collection_name: "task"
@@ -8,14 +14,25 @@ exports.definition = {
 	},
 	extendModel: function(Model) {
 		_.extend(Model.prototype, {
-			// extended functions and properties go here
+			// 妥当性検査
+			validate : function (attr) {
+				if((attr.what).length <= 0){
+					return "Error : what is not input.";
+				}
+				if((attr.limitDate).length <= 0){
+					return "Error : limitDate is not input.";
+				}
+			}
 		});
 
 		return Model;
 	},
 	extendCollection: function(Collection) {
 		_.extend(Collection.prototype, {
-			// extended functions and properties go here
+			// 並び替え
+			comparator : function (task) {
+				return task.get('limitDate');
+			}
 		});
 
 		return Collection;
