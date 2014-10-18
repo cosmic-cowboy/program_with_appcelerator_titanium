53ebcc659f4d0dddc0d40cc4e4824b0236e59d2f
hiroo takaku <slgerkamp@gmail.com>
2014-10-18 16:15:46 +0900

    Chapter09 Alloyによるアプリ作成（タスク管理）：追加画面作成

--- chapter09/TaskManage/app/controllers/add.js
+++ chapter09/TaskManage/app/controllers/add.js
@@ -1,1 +1,63 @@
-var args = arguments[0] || {};
+function showSliderValue (e) {
+	$.sliderValue.text = String.format("%3.0f", e.value);
+}
+
+function checkDate (argument) {
+	var dateStr = $.inputLimitDate.value;
+	if(dateStr.length === 0){
+		return true;
+	}
+
+	if(!dateStr.match(/^\d{4}-\d{2}-\d{2}$/)){
+		return false;
+	}
+	var vYear  = dateStr.substr(0, 4);
+	var vMonth = dateStr.substr(5, 2) - 1;
+	var vDay   = dateStr.substr(8, 2);
+
+	if(vMonth >= 0 && vMonth <= 11 &&
+		vDay >= 1 && vDay <= 31){
+
+		var vDt = new Date(vYear, vMonth, vDay);
+
+		// 日付が有効で、設定した値と同じである
+		// なんかこの比較方法いやだな
+		if((vDt) &&
+			(vDt.getFullYear() == vYear &&
+			vDt.getMonth() == vMonth &&
+			vDt.getDate() == vDay)
+		){
+			return true;
+		}
+	}
+	return false;
+}
+
+function saveTask () {
+	if(checkDate() === false){
+		alert("日付を正しく入力してください");
+		$.inputLimitDate.focus();
+		return false;
+	}
+
+	var mTask = Alloy.createModel("task", {
+		what       : $.inputWhat.value,
+		detail     : $.inputDetail.value,
+		limitDate  : $.inputLimitDate.value,
+		importance : Number($.sliderValue.text),
+		status     : 0
+	});
+
+	if(mTask.isValid()){
+		mTask.save();
+		$.addWin.close({
+			animated : true
+		});
+		Alloy.Collections.task.fetch();
+	} else {
+		mTask.destroy();
+		alert("保存ができません");
+	}
+}
+
+
--- chapter09/TaskManage/app/controllers/task.js
+++ chapter09/TaskManage/app/controllers/task.js
@@ -1,1 +1,37 @@
-var args = arguments[0] || {};
+function addTask(){
+	var addWin, index;
+
+	// Alloy.Globals.currentTab に タブへの参照
+	// 参照がない場合は、indexコントローラから、taskTabへの参照を取得
+	if(Alloy.Globals.currentTab === undefined){
+
+		index = Alloy.createController("index");
+		Alloy.Globals.currentTab = index.getView("taskTab");
+
+	}
+
+	// addWinの変数にAddコントローラからのID=addWinを持つWindowを取得する
+	addWin = Alloy.createController("add").getView("addWin");
+	Alloy.Globals.currentTab.open(addWin);
+
+}
+
+
+function transformFunction (model) {
+	var transform, limitTime, importance;
+
+	transform = model.toJSON();
+
+	limitDate = transform.limitDate;
+	transform.limitDate = "期日：" + limitDate;
+	importance = transform.importance;
+	transform.importance = "重要度：" + importance;
+
+	return transform;
+}
+
+function filterFunction (collection) {
+	return collection.where({
+		status : 0
+	});
+}
