9f532bc94ccbeca2b33e6197d23cf417638c534d
hiroo takaku <slgerkamp@gmail.com>
2014-10-25 17:57:09 +0900

    Chapter09 Alloyによるアプリ作成（タスク管理）：完了画面作成

--- chapter09/TaskManage/app/controllers/done.js
+++ chapter09/TaskManage/app/controllers/done.js
@@ -1,1 +1,16 @@
-var args = arguments[0] || {};
+function transformFunction(model) {
+	var transform, limitTime, importance;
+	transform = model.toJSON();
+
+	limitDate = transform.limitDate;
+	transform.limitDate = "期日:" + limitDate;
+	importance = transform.importance;
+	transform.importance = "重要度:" + importance;
+	return transform;
+}
+
+function filterFunction(collection) {
+	return collection.where({
+		status: 1
+	});
+}
