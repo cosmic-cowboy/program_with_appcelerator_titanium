4e7af55a1f4c47a58726523b32330bef4ba42141
hiroo takaku <slgerkamp@gmail.com>
2014-10-26 22:58:42 +0900

    Chapter07 Popoverを活用したアプリ作成：popoverを定義

--- chapter07/popoveriPad/app/controllers/index.js
+++ chapter07/popoveriPad/app/controllers/index.js
@@ -1,1 +1,16 @@
+
+// popoverは抽象化できるのでは？
+function popoverQuantity(){
+	var popover = Alloy.createController('quantity_popover').getView();
+    popover.show({view:$.quantityField});
+}
+function popoverName(){
+	var popover = Alloy.createController('product_name_popover').getView();
+    popover.show({view:$.nameField});
+}
+function popoverDate(){
+	var popover = Alloy.createController('product_date_popover').getView();
+    popover.show({view:$.dateField});
+}
+
 $.index.open();
