b152616c063f7de46ac93db60b232d41459500cf
hiroo takaku <slgerkamp@gmail.com>
2014-10-27 23:19:31 +0900

    Chapter08 電子行政オープンデータを利用したアプリ（walking）：xmlの読み込み

--- chapter08/walkingSakai/app/controllers/index.js
+++ chapter08/walkingSakai/app/controllers/index.js
@@ -1,5 +1,20 @@
+
+function readXML(){
+	var xmlFile = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'xml/walk.xml');
+	var xmlcontent = xmlFile.read();
+	var xmltext = xmlcontent.text;
+	var xmlDomDoc = Ti.XML.parseString(xmltext);
+
+	var curve = xmlDomDoc.getElementById('ls3');
+	var coordinateTags = curve.getElementsByTagName('coordinate');
+	for(var i = 0; i < coordinateTags.length; i++){
+		var coordinate = coordinateTags.item(i).textContent;
+		Ti.API.info(coordinate + '(' + i + ')');
+	}
+}
+
 function doClick(e) {
-    alert($.label.text);
+    readXML();
 }
 
 $.index.open();
