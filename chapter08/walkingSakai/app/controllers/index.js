98308b81b1cbf11f65d60793afd739d55a2e431d
hiroo takaku <slgerkamp@gmail.com>
2014-10-28 23:15:47 +0900

    Chapter08 電子行政オープンデータを利用したアプリ（walking）：ツールバーの設置

--- chapter08/walkingSakai/app/controllers/index.js
+++ chapter08/walkingSakai/app/controllers/index.js
@@ -1,3 +1,44 @@
+// toolbarのアイテムリスト
+var toolItems = [
+	{title:"衛生", type:Alloy.Globals.Map.SATELLITE_TYPE},
+	{title:"標準", type:Alloy.Globals.Map.STANDARD_TYPE},
+	{title:"ハイブリッド",type:Alloy.Globals.Map.HYBRID_TYPE},
+	{title:"+",zoom:1},
+	{title:"-",zoom:-1}
+];
+// 空きスペース
+var flexSpace = Titanium.UI.createButton({
+	systemButton : Titanium.UI.iPhone.SystemButton.FLEXIBLE_SPACE
+});
+
+var items = [];
+
+items.push(flexSpace);
+
+_.map(toolItems, function(toolItem) {
+	if(toolItem.type){
+		var button = Titanium.UI.createButton({
+			title:toolItem.title,
+			style:Titanium.UI.iPhone.SystemButtonStyle.BORDERED
+		});
+		button.addEventListener('click', function() {
+			$.mapview.setMapType(toolItem.type);
+		});
+		items.push(button);
+	} else if(toolItem.zoom){
+		var zoombutton = Titanium.UI.createButton({
+			title:toolItem.title,
+			style:Titanium.UI.iPhone.SystemButtonStyle.BORDERED
+		});
+		zoombutton.addEventListener('click', function() {
+			$.mapview.zoom(toolItem.zoom);
+		});
+		items.push(zoombutton);
+
+	}
+	items.push(flexSpace);
+});
+$.mapToolBar.items = items;
 
 function readXML(){
 	var xmlFile = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'xml/walk.xml');
