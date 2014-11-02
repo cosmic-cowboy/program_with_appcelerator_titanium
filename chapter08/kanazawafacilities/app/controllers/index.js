e5f17b2fb829723f47d2b6a2c26ac8790c203242
hiroo takaku <slgerkamp@gmail.com>
2014-11-02 10:26:00 +0900

    Chapter08 電子行政オープンデータを利用したアプリ（kanazawaFacilities）：金沢市のオープンデータの取得・描画処理

--- chapter08/kanazawafacilities/app/controllers/index.js
+++ chapter08/kanazawafacilities/app/controllers/index.js
@@ -1,3 +1,79 @@
+
+//
+// 初期処理
+//
+var HTTP = require('lib/HttpRequest');
+var genres = [];
+
+
+// SplitWindow orientation（デバイスの向き）対応
+// http://docs.appcelerator.com/titanium/3.0/#!/api/Titanium.UI.iPad.SplitWindow
+$.index.addEventListener('visible',function(e){
+
+	if (e.view == 'detail'){
+		// portrait
+		e.button.title = "Master";
+		$.index.detailView.getWindow().leftNavButton = e.button;
+	} else if (e.view == 'master'){
+		// landscape
+		$.index.detailView.getWindow().leftNavButton = null;
+	}
+
+});
+
+setGenres();
+
+$.index.open();
+
+
+
+
+//
+// 関数宣言
+//
+
+
+// ジャンル取得
+function setGenres() {
+
+	var request = {
+		url : "https://infra-api.city.kanazawa.ishikawa.jp/v1/genres/list.json",
+		onload  : function () {
+			// jsonの取得
+			var jsondata = JSON.parse(this.responseText);
+			_.map(jsondata.genres,function (genre) {
+				
+				genres.push({
+					id  : genre.id,
+					name: genre.name
+				});
+			});
+
+			// json取り出しと表示追加
+			// 外出し可能というか、依存性を低めるためには出したほうが良さそう
+			var data = [];
+			_.map(genres,function (genre) {
+				data.push({
+					properties : {
+						title : genre.name
+					}
+				});
+			});
+			var section = Ti.UI.createListSection({items:data});
+			$.listView.sections = [section];
+		},
+		onerror : function () {
+			alert('error');
+		}
+	};
+	HTTP.get(request);
+}
+
+
+//
+// Alloy要素のイベンドアクション
+//
+
 // 地図を変更
 function changeMap(e){
 	// 地図を変更
@@ -15,6 +91,47 @@ function deleteMap(){
 	$.mapview.removeAllAnnotations();
 }
 
+// 施設取得
+function setFacilities (e) {
+	var genre = genres[e.itemIndex].id;
+	var title = genres[e.itemIndex].name;
+	var request = {
+		url : "https://infra-api.city.kanazawa.ishikawa.jp/v1/facilities/search.json?count=100&genre=" + genre,
+		onload  : function () {
 
+			var jsondata = JSON.parse(this.responseText);
+			var facilities = [];
+			_.map(jsondata.facilities,function (facility) {
+				
+				facilities.push({
+					name     : facility.name,
+					address  : facility.address,
+					latitude : facility.coordinates.latitude,
+					longitude: facility.coordinates.longitude
+				});
+			});
 
-$.index.open();
+			// json取り出しと表示追加
+			// 外出し可能というか、依存性を低めるためには出したほうが良さそう
+			var anno = [];
+			_.map(facilities,function (facility) {
+				anno.push(
+					Alloy.Globals.Map.createAnnotation({
+						title     : facility.name,
+						subtitle  : facility.address,
+						latitude  : facility.coordinates.latitude,
+						longitude : facility.coordinates.longitude,
+						pincolor  : Alloy.Globals.Map.ANNOTATION_RED
+					})
+				);
+			});
+			$.mapview.addAnnotations(anno);
+			// body...
+		},
+		onerror : function () {
+			alert('error');
+		}
+	};
+	HTTP.get(request);
+}
+
