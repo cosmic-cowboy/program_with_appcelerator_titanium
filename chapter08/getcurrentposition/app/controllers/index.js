72cae526b9eec4ec67e595a11d9791ad779d0762
hiroo takaku <slgerkamp@gmail.com>
2014-11-02 11:23:36 +0900

    Chapter08 電子行政オープンデータを利用したアプリ（getcurrentposition）：現在地取得に必要な処理を実装

--- chapter08/getcurrentposition/app/controllers/index.js
+++ chapter08/getcurrentposition/app/controllers/index.js
@@ -1,5 +1,35 @@
-function doClick(e) {
-    alert($.label.text);
-}
+getCurrent();
+
+// 継続的な位置情報取得
+// Titanium.Geolocation.addEventListener('location', function(e){
+// 	if (e.error) {
+// 		Ti.API.error('Error: ' + e.error);
+// 	} else {
+// 		Ti.API.info(e.coords);
+// 		showMap(e);
+// 	}
+// });
 
 $.index.open();
+
+// 現在地取得
+function getCurrent(){
+	Titanium.Geolocation.getCurrentPosition(function(e) {
+		if (e.error) {
+			Ti.API.error('Error: ' + e.error);
+		} else {
+			Ti.API.info(e.coords);
+			showMap(e);
+		}
+	});
+}
+
+// マップへの設定処理
+function showMap(e){
+	$.mapview.region = {
+		latitude:e.coords.latitude,
+		longitude:e.coords.longitude,
+		latitudeDelta:0.02,
+		longitudeDelta:0.02
+	};
+}
